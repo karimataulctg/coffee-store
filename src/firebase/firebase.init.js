09355c4b0aa1d56b7bf7bed5153ee71b884c1563
karimataulctg <karimataulctg@gmail.com>
2024-12-02 08:16:43 +0600

    updated

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,19 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Initialize Firebase using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyB7PkMVeU8juvnSd2zPqsATUImToON2rHM",
-  authDomain: "coffee-store-54ccf.firebaseapp.com",
-  projectId: "coffee-store-54ccf",
-  storageBucket: "coffee-store-54ccf.firebasestorage.app",
-  messagingSenderId: "20867856939",
-  appId: "1:20867856939:web:9f2fcf0c8ed22e7eb5886e"
+  apiKey: "AIzaSyCvPonNZ1hxxPAEwjlsgT6MQscY1_jkuUw",
+  authDomain: "coffee-store-ef42d.firebaseapp.com",
+  projectId: "coffee-store-ef42d",
+  storageBucket: "coffee-store-ef42d.appspot.com",
+  messagingSenderId: "380889506021",
+  appId: "1:380889506021:web:667d43eed49845278db40c",
+  measurementId: "G-4VV2MN79LL"
 };
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
